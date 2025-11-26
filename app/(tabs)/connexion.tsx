340f14d7fc31c4fff71f6e2dac0e2c98b2e7aca1
hfatema <hfatema1888@gmail.com>
2025-11-26 18:05:49 -0500

    change

---
+++ app/(tabs)/connexion.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+
+const Profil = () => {
+return (
+    <View>
+    <Text>connexion</Text>
+
+
+    </View>
+);
+};
+
+export default Profil;
